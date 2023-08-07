669c7c538560f8e6f62e5f23d043d4e29452fde1
Zilo11 <137594239+Zilo11@users.noreply.github.com>
2023-08-07 05:37:30 +0100

    Update settings.py

--- zilo/settings.py
+++ zilo/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/item_images'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
